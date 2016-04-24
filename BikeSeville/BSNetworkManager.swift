68940467a0e3a298b5a5ecc379a29df23126b65d
dacioleal <dacioleal@gmail.com>
2016-04-24 19:13:30 +0200

    Singleton for networkManager created

---
+++ BikeSeville/BSNetworkManager.swift
@@ -0,0 +1,9 @@
+//
+//  BSNetworkManager.swift
+//  BikeSeville
+//
+//  Created by Dacio Leal Rodriguez on 24/4/16.
+//  Copyright © 2016 Dacio Leal Rodriguez. All rights reserved.
+//
+
+import Foundation
